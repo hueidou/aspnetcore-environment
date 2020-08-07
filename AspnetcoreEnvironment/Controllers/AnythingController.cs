517b5fd83622f5cec3e08321d559eceee36f3002
hueidou <hueidou163@gmail.com>
2020-08-07 16:32:57 +0800

    first commit

---
+++ AspnetcoreEnvironment/Controllers/AnythingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetcoreEnvironment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnythingController : ControllerBase
+    {
+        // GET api/anything
+        [HttpGet]
+        public ActionResult<dynamic> Get()
+        {
+            string remoteIpAddress = HttpContext.Connection.RemoteIpAddress.ToString();
+            string localIpAddress = HttpContext.Connection.LocalIpAddress.ToString();
+
+            return new { HttpContext.Request.Headers, remoteIpAddress, localIpAddress };
+        }
+    }
+}
